47038a221553166c374c10bfdb2f75737d91d157
Willian Antunes <willian.lima.antunes@gmail.com>
2021-06-13 12:36:48 -0300

    fix: allow createdat and updatedat to be properly set

--- src/Infrastructure/Database/AppDbContext.cs
+++ src/Infrastructure/Database/AppDbContext.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TicTacToeCSharpPlayground.Core.Models;
 
@@ -34,6 +38,37 @@ namespace TicTacToeCSharpPlayground.Infrastructure.Database
             }
 
             CreateManyToManyBetweenPlayerAndBoard();
+        }
+
+        public override int SaveChanges()
+        {
+            AutomaticallyAddCreatedAndUpdatedAt();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            AutomaticallyAddCreatedAndUpdatedAt();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void AutomaticallyAddCreatedAndUpdatedAt()
+        {
+            var entitiesOnDbContext = ChangeTracker.Entries<StandardEntity>();
+
+            if (entitiesOnDbContext is null)
+                return;
+
+            foreach (var item in entitiesOnDbContext.Where(t => t.State == EntityState.Added))
+            {
+                item.Entity.CreatedAt = DateTime.Now;
+                item.Entity.UpdatedAt = DateTime.Now;
+            }
+
+            foreach (var item in entitiesOnDbContext.Where(t => t.State == EntityState.Modified))
+            {
+                item.Entity.UpdatedAt = DateTime.Now;
+            }
         }
     }
 }
